87e0427cb2c9f7e4de72aebf068cfe0f9c0a2b49
juncid <juncid@gmail.com>
2020-06-01 23:58:41 -0400

    Add Group transition for a list of contacts

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Modal</router-link> |
+      <router-link to="/list">List</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <transition name="slide-fade" mode="out-in">
@@ -18,33 +19,22 @@
   text-align: center;
   color: #2c3e50;
 }
-
 button {
-  width: 5em;
-  height: 2em;
+  height: 2.2em;
   margin: 0.5em;
-  border-radius: 5px;
+  border-radius: 0.3em;
   background: linear-gradient(to right, #16c0b0, #84cf6a);
-  font-size: 1em;
   color: white;
-  border: none;
-  outline: none;
-  cursor: pointer;
 }
-
-#nav {
-  padding: 30px;
+input {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 30%;
+  height: 1.8em;
+  padding: 0.5em;
+  margin: 0.2em;
+  font: 0.8em 'Avenir', Helvetica, sans-serif;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
 .modal {
   width: 250px;
   height: 200px;
@@ -54,10 +44,29 @@ button {
   border-radius: 2%;
   background-color: #e0e0e0;
 }
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+p {
+  font-size: 0.8em;
+}
+::placeholder {
+  color: #2c3e50;
+}
+ul {
+  list-style-type: none;
+}
 /*** TRANSITIONS ***/
 .fade-enter {
-   opacity: 0;
- }
+  opacity: 0;
+}
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s ease-out;
@@ -79,5 +88,16 @@ button {
 .slide-fade-leave-to {
   transform: translateX(-10px);
   opacity: 0;
+}
+
+.slide-up-enter {
+  transform: translateY(10px);
+  opacity: 0;
+}
+.slide-up-enter-active {
+  transition: all 0.2s ease;
+}
+.slide-up-move {
+  transition: transform 0.5s ease-out;
 }
 </style>
---
+++ src/views/List.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <input type="text" v-model="newContact" placeholder="Name" />
+    <button @click="addContact">Add a Contact</button>
+    <button @click="sortContacts">Sort</button>
+    <transition-group name="slide-up" tag="ul" appear>
+      <li v-for="contact in contacts" :key="contact">
+        {{ contact }}
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data() {
+    return {
+      newContact: '',
+      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
+    }
+  },
+  methods: {
+    addContact() {
+      this.contacts.push(this.newContact)
+      this.newContact = ''
+    },
+    sortContacts() {
+      this.contacts.sort()
+    }
+  }
+}
+</script>
+
+<style scoped></style>
