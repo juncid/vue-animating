f7827119edb8dd81071785781100d34585936942
juncid <juncid@gmail.com>
2020-06-03 16:12:19 -0400

    state, timelines and nested timelines using GSAP

--- src/router.js
+++ src/router.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Modal from './views/Modal.vue'
+import Modal from './views/Modal'
 import List from './views/List'
 import Cards from './views/Cards'
 import Drawer from './views/Drawer'
+import Simple from './views/Simple'
+import Stagger from './views/Stagger'
+import State from './views/State'
+import Timeline from './views/Timeline'
+import Master from './views/Master'
 
 Vue.use(Router)
 
@@ -30,6 +35,31 @@ export default new Router({
       path: '/cards',
       name: 'cards',
       component: Cards
+    },
+    {
+      path: '/simple',
+      name: 'simple',
+      component: Simple
+    },
+    {
+      path: '/stagger',
+      name: 'stagger',
+      component: Stagger
+    },
+    {
+      path: '/state',
+      name: 'state',
+      component: State
+    },
+    {
+      path: '/timeline',
+      name: 'timeline',
+      component: Timeline
+    },
+    {
+      path: '/master',
+      name: 'master',
+      component: Master
     },
     {
       path: '/about',
